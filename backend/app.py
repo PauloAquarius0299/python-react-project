d98109c5b2dc74e42c742fe135b1506a53d3156d
Paulo Cesar <paulocgsantos99@gmail.com>
2024-05-14 19:03:22 -0300

    deploy now

--- backend/app.py
+++ backend/app.py
@@ -12,11 +12,11 @@ app.config["SQLACHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-frontend_folder = os.path.join(os.getcwd(), "..", "frontend")
+frontend_folder = os.path.join(os.getcwd(),"..","frontend")
 dist_folder = os.path.join(frontend_folder,"dist")
 
 #Server static files the 'dist' folder under the 'frontend' directory
-@app.route("/",defaults={"filename": ""})
+@app.route("/",defaults={"filename":""})
 @app.route("/<path:filename>")
 def index(filename):
   if not filename:
